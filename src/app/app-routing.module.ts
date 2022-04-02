f2cdd2eaa62b60f66464aeee27b1ae26a6b64de5
Jonathan <jonathanoudshoorn@gmail.com>
2022-04-02 20:48:25 +0200

    adding bootstrap and an nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CvComponent } from './cv/cv.component';
 
 const routes: Routes = [
+  { path: '',   redirectTo: '/cv', pathMatch: 'full' },
+  { path: 'cv', component: CvComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
